a11fb4ac4b35aa6e00c0eb20e2ee96cdb3615c36
Sreyansi19 <sreyansimohanty602@gmail.com>
2024-09-10 17:05:49 +0530

    Initial commit

---
+++ crud-app/server/routes/userRoute.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { create, deleteUser, getAll, getOne, update } from "../controller/userController.js";
+
+const router = express.Router();
+
+router.post("/create", create);
+router.get("/getall", getAll);
+router.get("/getone/:id",getOne);
+router.put("/update/:id",update);
+router.delete("/delete/:id", deleteUser);
+
+export default router;
